6d1cee3dbb0e1378bf4ea0bb1b094e5bd5e927d3
MrCroo <gabszon002@gmail.com>
2021-05-10 10:22:05 +0200

    Modified the checkbox classnames

--- src/ListItem.js
+++ src/ListItem.js
@@ -4,13 +4,17 @@ export default function ListItem({ item }) {
   const { done, name } = item;
 
   if (done) {
-    return <div className="custom-control custom-checkbox">
-  <input type="checkbox" className="custom-control-input" id="customCheck1" checked/>
-  <label className="custom-control-label" htmlFor="customCheck1" >{name}</label>
-</div>;
+    return (
+      <div>
+        <input type="checkbox" id="customCheck1" checked />
+        <label htmlFor="customCheck1">{name}</label>
+      </div>
+    );
   }
-  return <div className="custom-control custom-checkbox">
-  <input type="checkbox" className="custom-control-input" id="customCheck1" unchecked/>
-  <label className="custom-control-label" htmlFor="customCheck1" >{name}</label>
-</div>;
+  return (
+    <div>
+      <input type="checkbox" id="customCheck1" unchecked />
+      <label htmlFor="customCheck1">{name}</label>
+    </div>
+  );
 }
